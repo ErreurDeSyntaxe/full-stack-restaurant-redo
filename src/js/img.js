30bd9f72e58b9d5a0cbe9f7b9d190a25e9ff9049
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-12-12 16:43:33 +0800

    Add boilerplate JS

--- src/js/img.js
+++ src/js/img.js
@@ -1,7 +1,7 @@
 // How to add images thru JS using webpack
-import previewImg from '../../README/project-preview.png';
+// import previewImg from '../../README/project-preview.png';
 
-const image = document.createElement('img');
-image.src = previewImg;
+// const image = document.createElement('img');
+// image.src = previewImg;
 
-document.body.appendChild(image);
+// document.body.appendChild(image);
---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import '../css/style.css';
+
+console.log('Hi! My name is Xavier.');
