dfc558e1f1cf6e7776524b900564eb2cdc68fb61
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-12-13 13:17:11 +0800

    Add imgs

--- src/js/pages/menu.js
+++ src/js/pages/menu.js
@@ -1,5 +1,29 @@
 'use strict';
 
+import poutine from '../../img/poutine.jpg';
+import tourtiere from '../../img/tourtiere.jpg';
+import justWater from '../../img/just-water.jpg';
+import iceWater from '../../img/ice-water.jpg';
+import warmWater from '../../img/warm-water.jpg';
+import cheese from '../../img/extra-cheese.jpg';
+import sauce from '../../img/extra-gravy.jpg';
+
+const imgPoutine = document.createElement('img');
+const imgTourtier = document.createElement('img');
+const imgJustWater = document.createElement('img');
+const imgIceWater = document.createElement('img');
+const imgWarmWater = document.createElement('img');
+const imgCheese = document.createElement('img');
+const imgSauce = document.createElement('img');
+
+imgPoutine.src = poutine;
+imgTourtier.src = tourtiere;
+imgJustWater.src = justWater;
+imgIceWater.src = iceWater;
+imgWarmWater.src = warmWater;
+imgCheese.src = cheese;
+imgSauce.src = sauce;
+
 export const buildMenu = function () {
   const markup = `
     <div id="menu">
@@ -7,52 +31,60 @@ export const buildMenu = function () {
       <div id="dishes">
         <h3>Dishes</h3>
         <img
-          src=""
+          src="${poutine}"
           class="wide-img"
           alt="Une autentique poutine québécoise"
         />
         <div class="menu-item">Petite Poutine: 10$</div>
         <div class="menu-item">Grosse Poutine: 15$</div>
-        <br /><img
-          src=""
+        <img
+          src="${tourtiere}"
           class="wide-img"
           alt="Une autentique tourtière québécoise"
         />
         <div class="menu-item">Petite Tourtière: 13$</div>
         <div class="menu-item">Grosse Tourtière: 18$</div>
-        <br />
+        
       </div>
 
       <div id="bevrages">
         <h3>Brevages</h3>
         <img
-          src=""
+          src="${justWater}"
           class="tall-img"
           alt="Un verre d'eau température ambiante"
         />
         <div class="menu-item">Eau: gratuite</div>
-        <br /><img src="" class="tall-img" alt="A verre d'eau glacée" />
+        <img 
+          src="${iceWater}" 
+          class="tall-img" 
+          alt="A verre d'eau glacée"
+        />
         <div class="menu-item">Eau froide: gratuite</div>
-        <br /><img src="" class="tall-img" alt="Une tasse d'eau chaude" />
+        <img 
+          src="${warmWater}" 
+          class="tall-img" 
+          alt="Une tasse d'eau chaude"
+        />
         <div class="menu-item">Eau Chaude: gratuite</div>
-        <br />
+        
       </div>
 
       <div id="extras">
         <h3>Extras</h3>
         <img
-          src=""
+          src="${cheese}"
           alt="Une portion de fromage en grains"
           class="square-img"
         />
         <div class="menu-item">Extra fromage: 5$</div>
-        <br /><img
-          src=""
+        <img
+          src="${sauce}"
           alt="Une portion de sauce poutine"
           class="square-img"
         />
         <div class="menu-item">Extra sauce: 4$</div>
-        <br />
+        
       </div>
     </div>`;
   document.getElementById('content').insertAdjacentHTML('beforebegin', markup);
